901ca84e3646d57cd2c7ddd35c58889130754243
Christopher Mark Gore <cgore@cgore.com>
2014-04-11 22:20:56 -0500

    Basic user input example.

--- 02-user-input/src/user_input/core.clj
+++ 02-user-input/src/user_input/core.clj
@@ -1,7 +1,7 @@
 (ns user-input.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (println "What's your name?")
+  (printf "Hello %s, how's it going?\n" (read-line)))
+  
