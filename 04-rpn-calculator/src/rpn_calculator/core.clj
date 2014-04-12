33a69073543083d73470f19af80f8831522b184f
Christopher Mark Gore <cgore@cgore.com>
2014-04-12 18:48:20 -0500

    Pulling the actions into a map.

--- 04-rpn-calculator/src/rpn_calculator/core.clj
+++ 04-rpn-calculator/src/rpn_calculator/core.clj
@@ -1,6 +1,24 @@
 (ns rpn-calculator.core
   (:use seesaw.core))
 
+(def all-actions {:decimal  {:button-text "."}
+                  :0        {:button-text "0"}
+                  :1        {:button-text "1"}  
+                  :2        {:button-text "2"}
+                  :3        {:button-text "3"}
+                  :4        {:button-text "4"}
+                  :5        {:button-text "5"}
+                  :6        {:button-text "6"}
+                  :7        {:button-text "7"}
+                  :8        {:button-text "8"}
+                  :9        {:button-text "9"}
+                  :+        {:button-text "+"}
+                  :-        {:button-text "-"}
+                  :*        {:button-text "*"}
+                  :division {:button-text "/"}
+                  :quit     {:button-text "Quit"}
+                  :space    {:button-text "SPC"}})
+
 (defn north-controls []
   (button :text "Quit!" :listen [:action (fn [e] (dispose! (all-frames)))]))
 
@@ -12,11 +30,12 @@
 
 (defn number-pad []
   (grid-panel :columns 4 :rows 4
-              :items (mapv (fn [x] (button :text x))
-                           ["7" "8" "9"   "/"
-                            "4" "5" "6"   "*"
-                            "1" "2" "3"   "-"
-                            "0" "." "SPC" "+"])))
+              :items (mapv (fn [action] (->> (-> all-actions action :button-text)
+                                            (button :text)))
+                           [:7 :8       :9     :division
+                            :4 :5       :6     :*
+                            :1 :2       :3     :-
+                            :0 :decimal :space :+])))
 
 (defn -main [& args]
   (invoke-later
