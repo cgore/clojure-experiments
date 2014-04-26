0bcf96f7e3dcf916e6071d5d9002994676d76349
Christopher Mark Gore <cgore@cgore.com>
2014-04-26 14:29:38 -0500

    Adding a stack and working value.

--- 04-rpn-calculator/src/rpn_calculator/core.clj
+++ 04-rpn-calculator/src/rpn_calculator/core.clj
@@ -1,40 +1,46 @@
 (ns rpn-calculator.core
+  (:require [clojure.string :as string])
   (:use seesaw.core))
 
-(def actions {:decimal  {:text "."
-                         :action (fn [e])}
-              :0        {:text "0"
-                         :action (fn [e])}
-              :1        {:text "1"
-                         :action (fn [e])}
-              :2        {:text "2"
-                         :action (fn [e])}
-              :3        {:text "3"
-                         :action (fn [e])}
-              :4        {:text "4"
-                         :action (fn [e])}
-              :5        {:text "5"
-                         :action (fn [e])}
-              :6        {:text "6"
-                         :action (fn [e])}
-              :7        {:text "7"
-                         :action (fn [e])}
-              :8        {:text "8"
-                         :action (fn [e])}
-              :9        {:text "9"
-                         :action (fn [e])}
-              :+        {:text "+"
-                         :action (fn [e])}
-              :-        {:text "-"
-                         :action (fn [e])}
-              :*        {:text "*"
-                         :action (fn [e])}
-              :division {:text "/"
-                         :action (fn [e])}
-              :quit     {:text "Quit"
-                         :action (fn [e] (dispose! (all-frames)))}
-              :space    {:text "SPC"
-                         :action (fn [e])}})
+(let [stack (ref [])
+      working (ref [])]
+  (defn add-digit [digit]
+    (dosync (alter working conj digit))))
+
+(def actions {:decimal        {:text "."
+                               :action (fn [e] (add-digit "."))}
+              :0              {:text "0"
+                               :action (fn [e] (add-digit 0))}
+              :1              {:text "1"
+                               :action (fn [e] (add-digit 1))}
+              :2              {:text "2"
+                               :action (fn [e] (add-digit 2))}
+              :3              {:text "3"
+                               :action (fn [e] (add-digit 3))}
+              :4              {:text "4"
+                               :action (fn [e] (add-digit 4))}
+              :5              {:text "5"
+                               :action (fn [e] (add-digit 5))}
+              :6              {:text "6"
+                               :action (fn [e] (add-digit 6))}
+              :7              {:text "7"
+                               :action (fn [e] (add-digit 7))}
+              :8              {:text "8"
+                               :action (fn [e] (add-digit 8))}
+              :9              {:text "9"
+                               :action (fn [e] (add-digit 9))}
+              :addition       {:text "+"
+                               :action (fn [e])}
+              :subtraction    {:text "-"
+                               :action (fn [e])}
+              :multiplication {:text "*"
+                               :action (fn [e])}
+              :division       {:text "/"
+                               :action (fn [e])}
+              :quit           {:text "Quit"
+                               :action (fn [e] (dispose! (all-frames)))}
+              :space          {:text "SPC"
+                               :action (fn [e])}})
 
 (defn button-for-action [action-key]
   (let [action (action-key actions)]
@@ -54,9 +60,9 @@
   (grid-panel :columns 4 :rows 4
               :items (mapv button-for-action
                            [:7 :8       :9     :division
-                            :4 :5       :6     :*
-                            :1 :2       :3     :-
-                            :0 :decimal :space :+])))
+                            :4 :5       :6     :multiplication
+                            :1 :2       :3     :subtraction
+                            :0 :decimal :space :addition])))
 
 (defn -main [& args]
   (invoke-later
